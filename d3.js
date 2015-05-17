f952f6c904b189969348be4d479ba3e891993b09
Louis Liu <louis.wenchao.liu@gmail.com>
2015-05-17 16:04:57 +0800

    use data binding other than dataset for svg.

--- d3.js
+++ d3.js
@@ -4,19 +4,18 @@ var svgContainer = d3.select("main")
     .attr("height", 300)
     .attr("border", 1);
 
-for (var i = 0; i < 3; i++) {
-  for (var j = 0; j < 3; j++) {
-    var index = i*3+j;
-    svgContainer.append("rect")
-      .attr("data-index", i*3+j)
-      .attr("x", i*100)
-      .attr("y", j*100)
+    svgContainer.selectAll('rect')
+      .data([0,1,2,3,4,5,6,7,8])
+      .enter()
+      .append("rect")
+      .attr("x", function(d){ return d%3*100; })
+      .attr("y", function(d){ return Math.floor(d/3)*100; })
       .attr("width", 100)
       .attr("height", 100)
       .attr("class", 'cell')
-      .on('click', function() {
-        if (boxes[Math.floor(this.dataset.index/3)][this.dataset.index%3] === value_Blank) {
-          placeX(this.dataset.index);
+      .on('click', function(index) {
+        if (boxes[Math.floor(index/3)][index%3] === value_Blank) {
+          placeX(index);
 
           var myChoice = new Finder(boxes).findNextPlacment(value_O);
 
@@ -33,8 +32,6 @@ for (var i = 0; i < 3; i++) {
         }
       })
       ;
-  }
-}
 
 function placeO(position) {
   svgContainer.append("circle")
@@ -49,16 +46,16 @@ function placeO(position) {
 
 function placeX(position) {
   svgContainer.append("line")
-    .attr("x1", Math.floor(position/3) * 100 + 10)
-    .attr("y1", (position % 3) * 100 + 10)
-    .attr("x2", Math.floor(position/3) * 100 + 90)
-    .attr("y2", (position % 3) * 100 + 90)
+    .attr("y1", Math.floor(position/3) * 100 + 10)
+    .attr("x1", (position % 3) * 100 + 10)
+    .attr("y2", Math.floor(position/3) * 100 + 90)
+    .attr("x2", (position % 3) * 100 + 90)
     .attr("class", "x");
   svgContainer.append("line")
-    .attr("x1", Math.floor(position/3) * 100 + 90)
-    .attr("y1", (position % 3) * 100 + 10)
-    .attr("x2", Math.floor(position/3) * 100 + 10)
-    .attr("y2", (position % 3) * 100 + 90)
+    .attr("y1", Math.floor(position/3) * 100 + 90)
+    .attr("x1", (position % 3) * 100 + 10)
+    .attr("y2", Math.floor(position/3) * 100 + 10)
+    .attr("x2", (position % 3) * 100 + 90)
     .attr("class", "x");
   boxes[Math.floor(position/3)][position%3] = value_X;
 }
